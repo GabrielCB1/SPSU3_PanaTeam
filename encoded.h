2bb32a24fe2fbbcd63759ab83b35b12b7d3b1a82
AlanHerreraTz <alanht17@AlanPC.localdomain>
2021-08-06 09:48:54 -0500

    done

---
+++ encoded.h
@@ -0,0 +1,13 @@
+#define MAXNUMWORDS 2048
+#define MAXWORDLENGTH 64
+
+//lives
+int numLives = 5;
+int numCorrect = 0;
+int oldCorrect = 0;
+
+//char input and guess
+char input[64];
+
+char guess[16];
+
